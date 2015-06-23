7906d9b364535ce701db05f7f0d6fa70f9590b9d
Philip Knape <philip.knape@gmail.com>
2015-06-23 09:24:47 +0200

    more stug

--- lib/spree/calculator/shipping/custom_shipping.rb
+++ lib/spree/calculator/shipping/custom_shipping.rb
@@ -6,6 +6,7 @@ module Spree
         preference :with_frame,        :decimal,  default: 0
         preference :without_frame,     :decimal,  default: 0
 
+
         def self.description
           'MGT'
         end
@@ -15,7 +16,22 @@ module Spree
         # As order_or_line_items we always get line items, as calculable we have
         # Coupon, ShippingMethod or ShippingRate.
         def compute_package(package)
+          
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
+          puts 'run this package once'
 
+          compute_from_price(total(package))
+
+        end
+
+        def compute_from_price(package)
           line_items = package.order.line_items
           # total_price, total_weight, shipping = compute_total_price, compute_total_weight, 0
 
@@ -33,48 +49,9 @@ module Spree
             total = 0
           end
 
-          total
-
-          # [shipping, prices[compute_index(total_weight)], handling_fee(total_price)].compact.sum
+          return total
         end
 
-        # private
-
-        # def compute_total_weight
-        #   line_items.map do |item|
-        #     weight = item.variant.weight > 0 ? item.variant.weight : preferred_default_weight
-        #     item.quantity * weight
-        #   end.reduce(:+)
-        # end
-
-        # def compute_total_price
-        #   line_items.map { |item| item.price * item.quantity }.reduce(:+)
-        # end
-
-        # def compute_index(total_weight)
-        #   index = weights.length - 2
-        #   while index >= 0
-        #     break if total_weight > weights[index]
-        #     index -= 1
-        #   end
-        #   index + 1
-        # end
-
-        # def item_within_bounds?(weight)
-        #   weight && weight > preferred_max_item_weight
-        # end
-
-        # def handling_fee(total_price)
-        #   preferred_handling_max < total_price ? 0 : preferred_handling_fee
-        # end
-
-        # def prices
-        #   @prices ||= preferred_price_table.split.map(&:to_f)
-        # end
-
-        # def weights
-        #   @weights ||= preferred_weight_table.split.map(&:to_f)
-        # end
       end
     end
   end
