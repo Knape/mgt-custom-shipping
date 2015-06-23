5ff03dc505e8f4ef8e4aa86043dd868bb4031b3f
Philip Knape <philip.knape@gmail.com>
2015-06-23 13:59:51 +0200

    last test

--- lib/spree/calculator/shipping/custom_shipping.rb
+++ lib/spree/calculator/shipping/custom_shipping.rb
@@ -16,60 +16,39 @@ module Spree
         # As order_or_line_items we always get line items, as calculable we have
         # Coupon, ShippingMethod or ShippingRate.
         def compute_package(package)
-          
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-          puts 'run this package once'
-
-          puts package.inspect
-
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
-          puts 'end this package once'
 
           line_items = package.order.line_items
           # total_price, total_weight, shipping = compute_total_price, compute_total_weight, 0
 
           cost = 0
-          puts cost
-          puts 'first'
+          frames = 0
 
           line_items.each do |item|
             if (item.variant.sku.length > 4)
-              cost = self.preferred_with_frame
-              puts cost
-              puts 'if frame'
+              cost = (self.preferred_with_frame / 2)
+              break
             else
-              cost = self.preferred_without_frame
-              puts cost
-              puts 'in withoutframe'
+              cost = (self.preferred_without_frame / 2)
             end
+          end
+
+          puts package.order.ship_address.inspect
+
+          line_items.each do |item|
+            if (item.variant.sku.length > 4)
+              frames +=1;
+            end
+          end
+
+          if (frames >= 2)
+            cost = 0
           end
 
           if (package.order.ship_address.address2.include? "pickup: true")
             cost = 0
-            puts 'if free shipping'
           end
 
-          puts cost
-          puts 'end'
           cost.to_f
-        end
-
-        def compute_from_price(package)
-          
         end
 
       end
