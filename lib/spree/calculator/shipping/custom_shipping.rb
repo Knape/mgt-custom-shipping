3aec8a26a6a8b08b898aaceb6fb915de1bf2f5a3
Philip Knape <philip.knape@gmail.com>
2015-06-23 08:53:55 +0200

    more stug

--- lib/spree/calculator/shipping/custom_shipping.rb
+++ lib/spree/calculator/shipping/custom_shipping.rb
@@ -5,44 +5,12 @@ module Spree
 
         preference :with_frame,        :decimal,  default: 0
         preference :without_frame,     :decimal,  default: 0
-        # preference :max_item_weight, :decimal, default: 18
-        # preference :max_item_width,  :decimal, default: 60
-        # preference :max_item_length, :decimal, default: 120
-        # preference :max_price,       :decimal, default: 120
-        # preference :handling_max,    :decimal, default: 50
-        # preference :handling_fee,    :decimal, default: 10
-        # preference :default_weight,  :decimal, default: 1
 
-        class << self
-          def description
-            Spree.t(:custom_shipping)
-          end
-
-          def register
-            super
-          end
+        def self.description
+          'MGT'
         end
 
-        # def item_oversized?(variant)
-        #   sizes = [
-        #     variant.width  ? variant.width  : 0,
-        #     variant.depth  ? variant.depth  : 0,
-        #     variant.height ? variant.height : 0
-        #   ].sort.reverse
-
-        #   return true if sizes[0] > preferred_max_item_length # Longest side.
-        #   return true if sizes[1] > preferred_max_item_width  # Second longest side.
-        #   false
-        # end
-
-        # Determine if weight or size goes over bounds.
-        # def available?(package)
-        #   package.order.variants.each do |variant|
-        #     return false if item_within_bounds?(variant.weight) # 18
-        #     return false if item_oversized?(variant)
-        #   end
-        #   true
-        # end
+       
 
         # As order_or_line_items we always get line items, as calculable we have
         # Coupon, ShippingMethod or ShippingRate.
@@ -51,21 +19,21 @@ module Spree
           line_items = package.order.line_items
           # total_price, total_weight, shipping = compute_total_price, compute_total_weight, 0
 
-          test = 0
+          total = 0
 
           line_items.each do |item|
             if (item.variant.sku.length > 4)
-              test = self.preferred_with_frame
+              total = self.preferred_with_frame
             else
               test = self.preferred_without_frame
             end
           end
 
           if (package.order.ship_address.address2.include? "pickup: true")
-            test = 0
+            total = 0
           end
 
-          return test
+          total
 
           # [shipping, prices[compute_index(total_weight)], handling_fee(total_price)].compact.sum
         end
